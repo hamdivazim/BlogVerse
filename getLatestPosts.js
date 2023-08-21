d9a0a6e053bd373e398a5b44649a378232b936b6
Hamd Waseem <coding.wizard4@gmail.com>
2023-08-21 07:10:56 +0100

    Create getLatestPosts.js

---
+++ getLatestPosts.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// Read the 'archive.json' file
+const archiveData = fs.readFileSync('archive.json', 'utf8');
+const archive = JSON.parse(archiveData);
+
+// Get the last twenty items from the 'posts' array
+const lastTwentyPosts = archive.posts.slice(-20);
+
+// Create a new object with the extracted posts
+const postsObject = {
+  posts: lastTwentyPosts,
+  num: archive.num
+};
+
+// Write the extracted posts to 'posts.json'
+fs.writeFileSync('posts.json', JSON.stringify(postsObject, null, 2));
